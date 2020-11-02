c113202a779cda78d550ff5a2da4d93ad61b84f5
Pieter van der Merwe <pieterv@icloud.com>
2020-11-02 13:06:08 +0200

    Working Read/Write

--- sparkfun_RFD77402.py
+++ sparkfun_RFD77402.py
@@ -1,10 +1,12 @@
 import board
+import time
+from micropython import const
+
 from adafruit_bus_device.i2c_device import I2CDevice
 
 RFD77402_ADDR                   = const(0x4C) # 7-bit unshifted default I2C Address
 
 # Register addresses
-
 _RFD77402_ICSR                   = const(0x00)
 _RFD77402_INTERRUPTS             = const(0x02)
 _RFD77402_COMMAND                = const(0x04)
@@ -28,208 +30,178 @@ _RFD77402_MODE_STANDBY           = const(0x10)
 _RFD77402_MODE_OFF               = const(0x11)
 _RFD77402_MODE_ON                = const(0x12)
 
-_CODE_VALID_DATA                 = const(0x00)
-_CODE_FAILED_PIXELS              = const(0x01)
-_CODE_FAILED_SIGNAL              = const(0x02)
-_CODE_FAILED_SATURATED           = const(0x03)
-_CODE_FAILED_NOT_NEW             = const(0x04)
-_CODE_FAILED_TIMEOUT             = const(0x05)
+CODE_VALID_DATA                 = const(0x00)
+CODE_FAILED_PIXELS              = const(0x01)
+CODE_FAILED_SIGNAL              = const(0x02)
+CODE_FAILED_SATURATED           = const(0x03)
+CODE_FAILED_NOT_NEW             = const(0x04)
+CODE_FAILED_TIMEOUT             = const(0x05)
 
-class sparkfun_rfd77402: 
+class rfd77402: 
 
-    def __init__(self, i2c, address=RFD77402_ADDR):
+    _debug = False
+
+    def __init__(self, i2c, address=RFD77402_ADDR, debug=False):
         self._device = I2CDevice(i2c, address)   
+        self._debug = debug
 
     def begin(self): 
-        if (self.get_chip_id() < 0xAD00):
+        if(self.get_chip_id() < 0xAD00):
             return False # Chip ID failed. Should be 0xAD01 or 0xAD02
 
         if (not self.goto_standby_mode()): 
             return False # Chip timed out before going to standby
 
-        # Drive INT_PAD high
-        setting = self._read(_RFD77402_ICSR)
-        setting &= 0b11110000 # clears writable bits
-        self._write(_RFD77402_ICSR, setting)
-        setting = self._read(_RFD77402_INTERRUPTS)
-        setting &= 0b00000000; # Clears bits
-        self.write(_RFD77402_INTERRUPTS, setting)
+        # # Drive INT_PAD high
+        # setting = self._read(_RFD77402_ICSR)
+        # setting &= 0b11110000 # clears writable bits
+        # self._write(_RFD77402_ICSR, setting)
+        # setting = self._read(_RFD77402_INTERRUPTS)
+        # setting &= 0b00000000 # Clears bits
+        # self.write(_RFD77402_INTERRUPTS, setting)
 
-        # Configure I2C Interface
-        self._write16(_RFD77402_CONFIGURE_I2C, 0x65) # 0b.0110.0101 = Address increment, auto increment, host debug, MCPU debug
+        # # Configure I2C Interface
+        # self._write16(_RFD77402_CONFIGURE_I2C, 0x65) # 0b.0110.0101 = Address increment, auto increment, host debug, MCPU debug
 
-        # Set initialization - Magic from datasheet. Write 0x05 to 0x15 location.
-        self._write_16(_RFD77402_CONFIGURE_PMU, 0x0500) # 0b.0000.0101.0000.0000 //Patch_code_id_en, Patch_mem_en
+        # # Set initialization - Magic from datasheet. Write 0x05 to 0x15 location.
+        # self._write_16(_RFD77402_CONFIGURE_PMU, 0x0500) # 0b.0000.0101.0000.0000 //Patch_code_id_en, Patch_mem_en
 
-        if (not self.goto_offmode()):
-            return False # MCPU never turned off
+        # if (not self.goto_offmode()):
+        #     return False # MCPU never turned off
 
-        # Read Module ID
-        # Skipped
+        # # Read Module ID
+        # # Skipped
 
-        # Read Firmware ID
-        #Skipped
+        # # Read Firmware ID
+        # #Skipped
 
-        # Set initialization - Magic from datasheet. Write 0x06 to 0x15 location.
-        self._write_16(_RFD77402_CONFIGURE_PMU, 0x0600) # MCPU_Init_state, Patch_mem_en
+        # # Set initialization - Magic from datasheet. Write 0x06 to 0x15 location.
+        # self._write_16(_RFD77402_CONFIGURE_PMU, 0x0600) # MCPU_Init_state, Patch_mem_en
 
-        if (not self.goto_on_mode()):
-            return False # MCPU never turned on
+        # if (not self.goto_on_mode()):
+        #     return False # MCPU never turned on
 
-        # ToF Configuration
-        # self.write_16(_RFD77402_CONFIGURE_A, 0xE100); # 0b.1110.0001 = Peak is 0x0E, Threshold is 1.
-        self.set_peak(0x0E) # Suggested values from page 20
-        self.set_threshold(0x01)
+        # # ToF Configuration
+        # # self.write_16(_RFD77402_CONFIGURE_A, 0xE100) # 0b.1110.0001 = Peak is 0x0E, Threshold is 1.
+        # self.set_peak(0x0E) # Suggested values from page 20
+        # self.set_threshold(0x01)
 
-        self._write_16(_RFD77402_CONFIGURE_B, 0x10FF)    # Set valid pixel. Set MSP430 default config.
-        self._write_16(_RFD77402_CONFIGURE_HW_0, 0x07D0) # Set saturation threshold = 2,000.
-        self._write_16(_RFD77402_CONFIGURE_HW_1, 0x5008) # Frequecy = 5. Low level threshold = 8.
-        self._write_16(_RFD77402_CONFIGURE_HW_2, 0xA041) # Integration time = 10 * (6500-20)/15)+20 = 4.340ms. Plus reserved magic.
-        self._write_16(_RFD77402_CONFIGURE_HW_3, 0x45D4) # Enable harmonic cancellation. Enable auto adjust of integration time Plus reserved magic.
+        # self._write_16(_RFD77402_CONFIGURE_B, 0x10FF)    # Set valid pixel. Set MSP430 default config.
+        # self._write_16(_RFD77402_CONFIGURE_HW_0, 0x07D0) # Set saturation threshold = 2,000.
+        # self._write_16(_RFD77402_CONFIGURE_HW_1, 0x5008) # Frequecy = 5. Low level threshold = 8.
+        # self._write_16(_RFD77402_CONFIGURE_HW_2, 0xA041) # Integration time = 10 * (6500-20)/15)+20 = 4.340ms. Plus reserved magic.
+        # self._write_16(_RFD77402_CONFIGURE_HW_3, 0x45D4) # Enable harmonic cancellation. Enable auto adjust of integration time Plus reserved magic.
 
-        if (not self.goto_standby_mode()): 
-            return False # Error - MCPU never went to standby
+        # if (not self.goto_standby_mode()): 
+        #     return False # Error - MCPU never went to standby
 
-        # Whew! We made it through power on configuration
+        # # Whew! We made it through power on configuration
 
-        # Get the calibration data via the 0x0006 mailbox command
-        # self.get_calibration_data() # Skipped
+        # # Get the calibration data via the 0x0006 mailbox command
+        # # self.get_calibration_data() # Skipped
         
-        # Put device into Standby mode
-        if (not self.goto_standby_mode()):
-            return False # Error - MCPU never went to standby
+        # # Put device into Standby mode
+        # if (not self.goto_standby_mode()):
+        #     return False # Error - MCPU never went to standby
 
-        # Now assume user will want sensor in measurement mode
+        # # Now assume user will want sensor in measurement mode
 
-        # Set initialization - Magic from datasheet. Write 0x05 to 0x15 location.
-        self._write_16(_RFD77402_CONFIGURE_PMU, 0x0500) # Patch_code_id_en, Patch_mem_en
+        # # Set initialization - Magic from datasheet. Write 0x05 to 0x15 location.
+        # self._write_16(_RFD77402_CONFIGURE_PMU, 0x0500) # Patch_code_id_en, Patch_mem_en
 
-        if (not self.goto_off_mode()):
-            return False # Error - MCPU never turned off
+        # if (not self.goto_off_mode()):
+        #     return False # Error - MCPU never turned off
 
-        # Write calibration data
-        # Skipped
+        # # Write calibration data
+        # # Skipped
 
-        # Set initialization - Magic from datasheet. Write 0x06 to 0x15 location.
-        self._write_16(_RFD77402_CONFIGURE_PMU, 0x0600) # MCPU_Init_state, Patch_mem_en
+        # # Set initialization - Magic from datasheet. Write 0x06 to 0x15 location.
+        # self._write_16(_RFD77402_CONFIGURE_PMU, 0x0600) # MCPU_Init_state, Patch_mem_en
 
-        if ( not self.goto_on_mode()):
-            return False # Error - MCPU never turned on
+        # if ( not self.goto_on_mode()):
+        #     return False # Error - MCPU never turned on
 
         return True
 
-    # Takes a single measurement and sets the global variables with new data
-    def take_measurement(self): 
-        pass
-
-    # Returns the local variable to the caller
-	def get_distance(self):
-        return 0
-
-	# Returns the number of valid pixels found when taking measurement
-    def get_valid_pixels(self):
-        pass
-
-	# Returns the qualitative value representing how confident the sensor is about its reported distance
-    def get_confidence_value(self):
-        pass
-
-	# Returns the qualitative value representing how confident the sensor is about its reported distance
-    def get_mode(self):
-        pass
-
-	# Tell MCPU to go to standby mode. Return true if successful
+    # Tell MCPU to go to standby mode. Return true if successful
     def goto_standby_mode(self):
-        pass
+        #Set Low Power Standby
+        self._write(_RFD77402_COMMAND, 0x90) # 0b.1001.0000 = Go to standby mode. Set valid command.
 
-	# Tell MCPU to go to off state. Return true if successful
-    def goto_off_mode(self):
-        pass
-
-	# Tell MCPU to go to on state. Return true if successful
-    def goto_nn_mode(self):
-        pass
-
-	# Tell MCPU to go to measurement mode. Takes a measurement. If measurement data is ready, return true
-    def goto_measurement_mode(self):
-        pass
-	
-	# Returns the VCSEL peak 4-bit value
-    def get_peak(self):
-        pass
-
-	# Sets the VCSEL peak 4-bit value
-    def set_peak(self, peakValue):
-        pass
-
-	# Returns the VCSEL Threshold 4-bit value
-    def get_threshold(self):
-        pass
-
-	#Sets the VCSEL Threshold 4-bit value
-    def set_threshold(self, threshold):
-        pass
-
-	# Returns the VCSEL Frequency 4-bit value
-    def get_frequency(self):
-        pass
-
-	# Sets the VCSEL Frequency 4-bit value
-    def set_frequency(self, threshold):
-        pass 
-
-	# Gets whatever is in the 'MCPU to Host' mailbox. Check ICSR bit 5 before reading.
-    def get_mailbox(self):
-        pass
-        
-    # Software reset the device
-	def reset(self):
-        pass
+        # Check MCPU_ON Status
+        for x in range(9):
+            if (self._read_16(_RFD77402_DEVICE_STATUS) & 0x001F) == 0x0000:
+                return True # MCPU is now in standby
+                time.sleep(0.01) # Suggested timeout for status checks from datasheet
+            else:
+                return False # Error - MCPU never went to standby        
 
 	# Returns the chip ID. Should be 0xAD01 or higher.
     def get_chip_id(self):
-        pass
+        return self._read_16(_RFD77402_MOD_CHIP_ID)
 
-	# Retreive 2*27 bytes from MCPU for computation of calibration parameters
-	# Reads 54 bytes into the calibration[] array
-	# Returns true if new cal data is loaded
-	def get_calibration_data(self):
-        pass
+    # Software reset the device
+    def reset(self):
+        self._write(_RFD77402_COMMAND, 1<<6)
+        time.sleep(0.1)  
 
     # Reads two bytes from a given location from the RFD77402
-	def _read_16(self, address):
+    def _read_16(self, address):
         # Read and return a 16-bit unsigned big endian value read from the
         # specified 16-bit register address.
+        if self._debug:
+            print("_read_16()")
+            print(address)
         with self._device:
-            self._device.write(bytes([(address >> 8) & 0xFF, address & 0xFF]), stop=False)
-            result = bytearray(2)
-            self._device.read_into(result)
-            return (result[0] << 8) | result[1]
+            # out_buffer = bytes([address])
+            out_buffer = bytes([address])
+            if self._debug:
+                print(out_buffer)
+            in_buffer = bytearray(2)
+            self._device.write_then_readinto(out_buffer, in_buffer)
+            if self._debug:
+                print(in_buffer)
+            return (in_buffer[1] << 8) | in_buffer[0]
 	
     # Reads from a given location from the RFD77402
-    def _read(self, address):        
+    def _read(self, address):
         # Read and return a byte from the specified 16-bit register address.
+        if self._debug:
+            print("_read()")
+            print(address)
         with self._device:
-            self._device.write(bytes([(address >> 8) & 0xFF, address & 0xFF]), stop=False)
-            result = bytearray(1)
-            self._device.read_into(result)
-            return result[0]
+            out_buffer = bytes([(address >> 8)])
+            if self._debug:
+                print(out_buffer)
+            in_buffer = bytearray(1)
+            self._device.write_then_readinto(out_buffer, in_buffer)
+            if self._debug:
+                print(in_buffer)
+            return in_buffer[0]
 
 	# Write a 16 bit value to a spot in the RFD77402
     def _write_16(self, address, data):
         # Write a 16-bit big endian value to the specified 16-bit register
         # address.
+        if self._debug:
+            print("_write_16()")
+            print(address)
+            print(data)
         with self._device:
-            self._device.write(bytes([(address >> 8) & 0xFF, address & 0xFF, (data >> 8) & 0xFF, data & 0xFF]))
+            out_buffer = bytes([address, data])
+            if self._debug:
+                print(out_buffer)
+            self._device.write(out_buffer)
 
     # Write a value to a spot in the RFD77402
-	def _write(self, address, data):
-        # Write 1 byte of data from the specified 16-bit register address.
+    def _write(self, address, data):
+        # Write 1 byte of data from the specified 16-bit register address.\
+        if self._debug:
+            print("_write_16()")
+            print(address)
+            print(data)
         with self._device:
-            self._device.write(bytes([(address >> 8) & 0xFF, address & 0xFF, data]))
-    
-    # Variables
-	_distance = 0
-	_validPixels = 0
-	_confidenceValue = 0
-	# _calibrationData[54] # Loaded by the 0x006 mailbox command
+            out_buffer = bytes([address, data])
+            if self._debug:
+                print(out_buffer)
+            self._device.write(out_buffer)
